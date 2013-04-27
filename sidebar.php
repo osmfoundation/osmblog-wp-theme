78e2c16202b2edb2a3045bc51391ce45d3171749
Harry Wood <github@onlineanimals.co.uk>
2013-04-27 18:12:18 +0100

    first cut
    made start. Based on wordpress twenty twelve theme. Trying to make it
    look like OSM diary pages, but needs more work

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,11 @@
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div id="secondary" class="widget-area" role="complementary">
+                    <div id="logo">
+                        <a href="http://blog.openstreetmap.org" title="OpenStreetMap blog home (recent posts)"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/osm-logo.png" width="135" height="135" alt="OSM logo" id="logo"/></a>
+                        <h1>OpenStreetMap blog</h1>
+                    </div>
+
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		</div><!-- #secondary -->
 	<?php endif; ?>
