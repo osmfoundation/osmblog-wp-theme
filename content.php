8dcd2f111308cc5b39c5e1eefa5dffccc4ed902c
Harry Wood <github@onlineanimals.co.uk>
2013-08-22 18:35:59 +0100

    move the 'leave a reply' link further down the page
    We're open to comments, but we don't need to encourage them quite some prominently. Move the reply link to the bottom of the page.

--- content.php
+++ content.php
@@ -23,11 +23,6 @@
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
 			<?php endif; // is_single() ?>
-			<?php if ( comments_open() ) : ?>
-				<div class="comments-link">
-					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-				</div><!-- .comments-link -->
-			<?php endif; // comments_open() ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
@@ -60,5 +55,10 @@
 					</div><!-- .author-description -->
 				</div><!-- .author-info -->
 			<?php endif; ?>
+			<?php if ( comments_open() ) : ?>
+				<div class="comments-link">
+					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+				</div><!-- .comments-link -->
+			<?php endif; // comments_open() ?>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
